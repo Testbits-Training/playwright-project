89507bd16ad211a8015fa884be7eebe8110c6c9b
Aerospade1 <eleqaljadid@gmail.com>
2022-07-18 10:53:20 +0800

    GithubAction Test1

---
+++ tests/dummyTest.spec.js
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Multilingual', () => {
+    test('test', async ({ page }) => {
+        await page.goto('https://www.wikipedia.org/');
+        await page.locator('strong:has-text("English")').click();
+        await expect(page).toHaveURL('https://en.wikipedia.org/wiki/Main_Page');
+        await page.locator('text=Main Page From Wikipedia, the free encyclopedia Jump to navigation Jump to searc').click();
+        await page.locator('div[role="main"] >> text=MediaWiki').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/MediaWiki');
+        await page.locator('main[role="main"] >> text=multilingual').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/Localisation');
+        await page.locator('text=Language names reference').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/Manual:Language#lang-code');
+      });
+});
+
+test.describe('Special Log', () =>{
+    test('test', async ({ page }) => {
+        await page.goto('https://www.wikipedia.org/');
+        await page.locator('strong:has-text("English")').click();
+        await expect(page).toHaveURL('https://en.wikipedia.org/wiki/Main_Page');
+        await page.locator('text=Main Page From Wikipedia, the free encyclopedia Jump to navigation Jump to searc').click();
+        await page.locator('div[role="main"] >> text=MediaWiki').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/MediaWiki');
+        await page.locator('main[role="main"] >> text=multilingual').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/Localisation');
+        await page.locator('text=Language names reference').click();
+        await expect(page).toHaveURL('https://www.mediawiki.org/wiki/Manual:Language#lang-code');
+      });
+})
+
+
