f6d7c19930da378cc8304221ae282bf0a7e101fb
Aerospade1 <eleqaljadid@gmail.com>
2022-07-18 13:21:11 +0800

    test3

--- tests/dummyTest.spec.js
+++ tests/dummyTest.spec.js
@@ -1,4 +1,4 @@
-const {expect} = require('@playwright/test');
+/*const {expect} = require('@playwright/test');
 
 
 test.describe('Multilingual', () => {
@@ -30,5 +30,5 @@ test.describe('Special Log', () =>{
         await expect(page).toHaveURL('https://www.mediawiki.org/wiki/Manual:Language#lang-code');
       });
 })
-
+*/
 
